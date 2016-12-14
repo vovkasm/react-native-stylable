4157df4087b5528f723a424720c44767829ae2fa
Vladimir Timofeev <vovkasm@gmail.com>
2016-12-14 17:58:07 +0300

    Add primitive test for StyleProvider, stylable components

--- tests/setup.js
+++ tests/setup.js
@@ -4,3 +4,9 @@ global.__DEV__ = true
 require('babel-register')()
 
 require('react-native-mock/mock')
+
+const jsdom = require('jsdom').jsdom
+
+global.document = jsdom('<body></body>')
+global.window = document.defaultView
+global.navigator = window.navigator
