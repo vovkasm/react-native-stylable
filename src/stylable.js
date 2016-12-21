a61db9e40e7db636ccf8320fc948a5300e9c2034
Vladimir Timofeev <vovkasm@gmail.com>
2016-12-21 13:19:09 +0300

    Move all logic to Node, rename props

--- src/stylable.js
+++ src/stylable.js
@@ -4,23 +4,39 @@ import hoistStatics from 'hoist-non-react-statics'
 import { Signal } from './mini-signals'
 
 class Node {
-  constructor (name, variant, parent, styleSheet) {
+  constructor (name, props, parent, styleSheet) {
     this.name = name
-    this.variant = variant
+    this.props = props
+    this.childProps = undefined
+    this.variant = props.variant
     this.parent = parent
     this.styleSheet = styleSheet
     this.changed = new Signal()
     this.path = undefined
     this.fullPath = undefined
-    this.resolvePath()
+    this.parentSubscription = undefined
+    this.changeCb = undefined
+    this.update()
   }
-  getStyleSheet () { return this.styleSheet }
-  setVariant (variant) {
-    this.variant = variant
-    this.resolvePath()
+  getChildProps () { return this.childProps }
+  subscribe (cb) {
+    this.changeCb = cb
+    if (this.parent !== undefined) {
+      this.parentSubscription = this.parent.changed.add(this.parentChanged, this)
+    }
+  }
+  parentChanged () {
+    this.update()
+  }
+  setProps (nextProps) {
+    this.props = nextProps
+    this.variant = nextProps.variant
+    this.update()
   }
   update () {
     this.resolvePath()
+    this.resolveChildProps()
+    if (this.changeCb) this.changeCb()
   }
   resolvePath () {
     const path = this.parent === undefined ? this.name : this.parent.fullPath + ' ' + this.name
@@ -37,6 +53,18 @@ class Node {
     this.fullPath = fullPath
     if (needNotify) this.changed.dispatch()
   }
+  resolveChildProps () {
+    const childProps = this.styleSheet.getProps(this.path, this.props, this.variant)
+    delete childProps.variant
+    this.childProps = childProps
+  }
+  dispose () {
+    if (this.parentSubscription !== undefined) {
+      this.parentSubscription.detach()
+      this.parentSubscription = undefined
+    }
+    this.changed.detachAll()
+  }
 }
 
 function getDisplayName (comp) {
@@ -52,10 +80,10 @@ export default function stylable (name) {
       static WrappedComponent = WrappedComponent
       static contextTypes = {
         styleSheet: PropTypes.object.isRequired,
-        styleSheetPath: PropTypes.object
+        styleNode: PropTypes.object
       }
       static childContextTypes = {
-        styleSheetPath: PropTypes.object.isRequired
+        styleNode: PropTypes.object.isRequired
       }
       static propTypes = {
         variant: PropTypes.any
@@ -63,51 +91,32 @@ export default function stylable (name) {
 
       constructor (props, ctx) {
         super(props, ctx)
-        this.path = new Node(name, props.variant, ctx.styleSheetPath, ctx.styleSheet)
-        this.parentSubscription = undefined
-        this.state = { childProps: this.getChildProps(props) }
+        this.node = new Node(name, props, ctx.styleNode, ctx.styleSheet)
+        this.state = { childProps: this.node.getChildProps() }
       }
 
       componentWillReceiveProps (nextProps) {
-        if (this.path.variant !== nextProps.variant) {
-          this.path.setVariant(nextProps.variant)
-        }
-        this.setState({ childProps: this.getChildProps(nextProps) })
+        this.node.setProps(nextProps)
       }
 
       componentDidMount () {
-        const parentPath = this.path.parent
-        if (parentPath !== undefined) {
-          this.parentSubscription = parentPath.changed.add(this.parentChanged, this)
-        }
+        this.node.subscribe(this.nodeChanged)
       }
 
       componentWillUnmount () {
-        if (this.parentSubscription !== undefined) {
-          this.parentSubscription.detach()
-          this.parentSubscription = undefined
-        }
+        this.node.dispose()
       }
 
-      parentChanged () {
-        this.path.update()
-        this.setState({ childProps: this.getChildProps(this.props) })
-      }
-
-      getChildProps (props) {
-        const childProps = this.path.getStyleSheet().getProps(this.path.path, props, props.variant)
-        delete childProps.variant
-        return childProps
-      }
-
-      getChildContext () {
-        return {
-          styleSheetPath: this.path
-        }
+      nodeChanged = () => {
+        this.setState({ childProps: this.node.getChildProps() })
       }
 
       render () {
         return React.createElement(WrappedComponent, this.state.childProps)
+      }
+
+      getChildContext () {
+        return { styleNode: this.node }
       }
     }
 
